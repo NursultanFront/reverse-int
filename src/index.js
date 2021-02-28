7138ae3aeab5bb41f76d522fa8df24b55a2aec71
NursultanFront <55098732+NursultanFront@users.noreply.github.com>
2021-02-28 14:59:26 +0500

    init : add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    let arr = str.split("");
+    arr.reverse();
+    str = arr.join("");
+    return parseInt(str);
+};
